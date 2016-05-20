37e7e66bb69b2b79d64a1071f3b9624603829786
Antonio M. Amaya <amac@tid.es>
2016-05-20 20:28:27 +0200

    [Test1]

---
+++ test1/index.js
@@ -0,0 +1,51 @@
+var parseSearch = function(aSearchStr) {
+  function _addValue(currValue, newValue) {
+    if (currValue === undefined) {
+      return newValue;
+    }
+    if (!Array.isArray(currValue)) {
+      currValue = [currValue];
+    }
+    currValue.push(newValue);
+    return currValue;
+  }
+
+  aSearchStr = window.decodeURIComponent(aSearchStr);
+  return aSearchStr.slice(1).split('&').
+    map(function(aParam) { return aParam.split('='); }).
+    reduce(function(aObject, aCurrentValue) {
+      var parName = aCurrentValue[0];
+      aObject.params[parName] = _addValue(aObject.params[parName], aCurrentValue[1] || null);
+      return aObject;
+    },
+           {
+             params: {},
+             getFirstValue: function(aParam) {
+               return Array.isArray(this.params[aParam]) ? this.params[aParam][0] : this.params[aParam];
+             }
+           }
+          );
+};
+
+function onload() {
+  var params = parseSearch(document.location.search || "").params;
+  console.log(params);
+  if (!params.apiKey || !params.sessionId || !params.token) {
+    document.getElementById('form-wrapper').classList.remove('hidden');
+  } else {
+    startOTTest(params);
+  }
+}
+
+function startOTTest(params) {
+  var session = OT.initSession(params.apiKey, params.sessionId);
+  session.on({
+  "connectionCreated": function(evt) {
+    console.log("Got a connection created event. Data: [" + evt.connection.data + "]");
+  },
+  "connectionDestroyed": function(evt) {
+    console.log("Got a connection destroyed event. Data: [" + evt.connection.data + "]");
+  }
+  });
+  session.connect(params.token);
+}
